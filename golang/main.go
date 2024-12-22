5ab54bf122e9c0795573f6a42db11f6cdca4a640
Phen705 <mogan250120@gmail.com>
2024-12-22 23:24:16 +0300

    Выполнил 3/6/7 работы

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"isuct.ru/informatics2022/LAB_6"
+	"isuct.ru/informatics2022/LAB_7"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Чернов Максим Александрович")
+	LAB_6.Lab6()
+	LAB_7.Lab7()
 }
