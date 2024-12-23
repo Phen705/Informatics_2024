40494576505e5382d15d1a290b68973ed7127916
Phen705 <mogan250120@gmail.com>
2024-12-23 21:50:44 +0300

    Выполнил 4 лабораторную

--- golang/main.go
+++ golang/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 
+	"isuct.ru/informatics2022/LAB_4"
 	"isuct.ru/informatics2022/LAB_6"
 	"isuct.ru/informatics2022/LAB_7"
 )
 
 func main() {
 	fmt.Println("Чернов Максим Александрович")
+	LAB_4.Lab4()
 	LAB_6.Lab6()
 	LAB_7.Lab7()
 }
